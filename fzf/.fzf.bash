7134212423c97782a45dda0f46281ae2bf53b112
James Menzies <james@menzies.io>
2021-12-12 11:06:04 -0600

    Initial commit of dotfiles

---
+++ fzf/.fzf.bash
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */Users/james/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/Users/james/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/Users/james/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/james/.fzf/shell/key-bindings.bash"
