5cf987a28e401a791a3b71f45fcebf70db22f064
James Menzies <james@menzies.io>
2023-05-05 14:31:55 +1000

    Migrated to starship and added home.nix

---
+++ zsh/aliases.zsh
@@ -0,0 +1,12 @@
+# Java & Gradle
+#alias gcb='gradle clean build'
+#alias use8='sdk use java 8.0.342-amzn'
+#alias use11='sdk use java 11.0.16-amzn'
+#alias use17='sdk use java 17.0.4-amzn'
+
+# ls
+unalias l
+alias l='exa --all --long --icons --color=auto --no-permissions --octal-permissions'
+
+# Zsh
+alias reload='source ~/.zshrc'
---
+++ zsh/functions.zsh
@@ -0,0 +1,26 @@
+source ~/.authenticate/authenticate.sh
+
+export OKTA_USERNAME=james.menzies
+export OKTA_MFA_OPTION=google
+export OKTA_PASSWORD_HELPER='op item get "IRESS - SSO" --fields password'
+export OKTA_TOTP_HELPER='op item get "IRESS - SSO" --otp'
+# export AUTHENTICATE_CACHE_DIR=~/.authenticate/cache/
+# export AUTHENTICATE_CACHE_MIN_TTL=43200
+
+# IRESS Login
+function auth () {
+  if [[ "$1" == "st" ]]; then
+    echo "Connecting to Staging AU"
+    eval $(op signin)
+    authenticate --to wealth-staging-au
+  else
+    echo "Connecting to Technet AU"
+    eval $(op signin)
+    authenticate
+  fi
+}
+
+# Colormap
+function colourmap() {
+  for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
+}
